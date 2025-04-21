e569700edbc6bad89057ea5dca3a79500e7519c2
ldiseno <jhonjpacheco2592@gmail.com>
2025-04-21 17:37:19 -0500

    Fix: import path module to resolve alias error

--- next.config.js
+++ next.config.js
@@ -1,3 +1,29 @@
+const path = require("path"); // <--- AGREGA ESTO AL INICIO
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  reactStrictMode: true,
+
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname, "src"), // usa `path` aquí
+    };
+    return config;
+  },
+};
+
+module.exports = nextConfig;
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
